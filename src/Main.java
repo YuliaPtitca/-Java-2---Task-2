5d339cbc21ac92a5660eea2244f24c3f86136a22
unknown <qa.yulia.ptitca@yandex.ru>
2022-07-12 21:41:00 +0300

    Corrected 'if' and code formate

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         int depositAmount = 1100;
         int scale = 100;
         int bonus;
-        if (depositAmount >= 1000) {
+        if (depositAmount > 1000) {
             bonus = depositAmount / scale;
         } else {
             bonus = 0;
         }
-            int totalAmount = firstAmount + depositAmount + bonus;
-            System.out.println(" Вы пополнили счет на " + depositAmount + " руб.");
-            System.out.println(" Ваш бонус составляет " + bonus + " руб.");
-            System.out.println(" Ваш баланс " + totalAmount + " руб.");
+        int totalAmount = firstAmount + depositAmount + bonus;
+        System.out.println(" Вы пополнили счет на " + depositAmount + " руб.");
+        System.out.println(" Ваш бонус составляет " + bonus + " руб.");
+        System.out.println(" Ваш баланс " + totalAmount + " руб.");
     }
 }
